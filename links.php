e79f3ce50a08c6e776ac665ab2cb8d8b9a50b047
dudaspiess <dudaspiess@gmail.com>
2018-10-12 18:09:57 -0300

    ädd new pages to the site

---
+++ links.php
@@ -0,0 +1,29 @@
+<?php
+  include 'header.html';
+?>
+        <h1 class="featurette-heading mt-2">Links Úteis</h1>
+        <hr class="featurette-divider mt-1 mb-5">
+        <div class="container">
+            <div class="col-md-6">
+                <h3 class="text-muted">Pesquisa</h4>
+                <ul>
+                    <li><a href="http://www.inep.gov.br" target="_blank">MEC/INEP</a></li>
+                    <li><a href="http://www.cnpq.br" target="_blank">CNPq</a></li>
+                    <li><a href="http://www.capes.gov.br" target="_blank">CAPES</a></li>
+                    <li><a href="http://www.finep.gov.br" target="_blank">FINEP</a></li>
+                    <li><a href="http://www.fapesc.sc.gov.br" target="_blank">FAPESC</a></li>
+                </ul>
+            </div>
+            <div class="col-md-6">
+                <h3 class="text-muted">Tecnologia</h4>
+                <ul>
+                    <li><a href="http://www.theverge.com" target="_blank">The Verge (Inglês)</a></li>
+                    <li><a href="http://www.canaltech.com.br" target="_blank">Canaltech</a></li>
+                    <li><a href="http://www.capes.gov.br" target="_blank">CAPES</a></li>
+                    <li><a href="http://www.finep.gov.br" target="_blank">FINEP</a></li>
+                    <li><a href="http://www.fapesc.sc.gov.br" target="_blank">FAPESC</a></li>
+                </ul>
+            </div>
+            <?php
+            include 'footer.html';
+          ?>
---
+++ projetos.php
@@ -0,0 +1,79 @@
+<?php
+  include 'header.html';
+?>
+        <h1 class="featurette-heading mt-2">Projetos Unisociesc Blumenau</h1>
+        <hr class="featurette-divider mt-1 mb-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="card h-100">
+                        <div class="card-body">
+                            <h4 class="card-title">Card title</h4>
+                            <h6 class="card-subtitle mb-2 text-muted"><span class="badge badge-secondary">New</span></h6>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="card-link">Card link</a>
+                            <a href="#" class="card-link">Another link</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card h-100">
+                        <div class="card-body">
+                            <h4 class="card-title">Card title</h4>
+                            <h6 class="card-subtitle mb-2"><span class="badge badge-secondary">New</span></h6>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="card-link">Card link</a>
+                            <a href="#" class="card-link">Another link</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card h-100">
+                        <div class="card-body">
+                            <h4 class="card-title">Card title</h4>
+                            <h6 class="card-subtitle mb-2 text-muted"><span class="badge badge-secondary">New</span></h6>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="card-link">Card link</a>
+                            <a href="#" class="card-link">Another link</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row mt-4">
+                <div class="col-md-4">
+                    <div class="card h-100">
+                        <div class="card-body">
+                            <h4 class="card-title">Card title</h4>
+                            <h6 class="card-subtitle mb-2 text-muted"><span class="badge badge-secondary">New</span></h6>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="card-link">Card link</a>
+                            <a href="#" class="card-link">Another link</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card h-100">
+                        <div class="card-body">
+                            <h4 class="card-title">Card title</h4>
+                            <h6 class="card-subtitle mb-2 text-muted"><span class="badge badge-secondary">New</span></h6>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="card-link">Card link</a>
+                            <a href="#" class="card-link">Another link</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card h-100">
+                        <div class="card-body">
+                            <h4 class="card-title">Card title</h4>
+                            <h6 class="card-subtitle mb-2 text-muted"><span class="badge badge-secondary">New</span></h6>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="card-link">Card link</a>
+                            <a href="#" class="card-link">Another link</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <?php
+            include 'footer.html';
+          ?>
