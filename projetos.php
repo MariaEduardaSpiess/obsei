2e8866040a4ef5ccafd59f6e433e9a5ae4cccbe6
Maria Eduarda Spiess <33469381+MariaEduardaSpiess@users.noreply.github.com>
2018-10-30 14:11:58 -0300

    Cleaned the lorem ipsums

--- projetos.php
+++ projetos.php
@@ -11,11 +11,11 @@
                     <div class="card-body">
                         <h4 class="card-title">Comunicação Estratégica e Competitiva</h4>
                         <h6 class="card-subtitle mb-2 text-muted"><span class="badge badge-primary">Comunicação</span></h6>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <!--<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                         <div class="bottom-links">
                             <a href="#" class="card-link">Card link</a>
                             <a href="#" class="card-link">Another link</a>
-                        </div>
+                        </div>-->
                     </div>
                 </div>
             </div>
@@ -24,11 +24,11 @@
                     <div class="card-body">
                         <h4 class="card-title">Conhecimento, Estratégia e Competitividade</h4>
                         <h6 class="card-subtitle mb-2"><span class="badge badge-primary">Administração</span></h6>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+<!--                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                         <div class="bottom-links">
                             <a href="#" class="card-link">Card link</a>
                             <a href="#" class="card-link">Another link</a>
-                        </div>
+                        </div> -->
                     </div>
                 </div>
             </div>
@@ -37,11 +37,11 @@
                     <div class="card-body">
                         <h4 class="card-title">Direito Empresarial e Sustentabilidade</h4>
                         <h6 class="card-subtitle mb-2 text-muted"><span class="badge badge-primary">Direito</span></h6>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+<!--                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                         <div class="bottom-links">
                             <a href="#" class="card-link">Card link</a>
                             <a href="#" class="card-link">Another link</a>
-                        </div>
+                        </div> -->
                     </div>
                 </div>
             </div>
@@ -52,11 +52,11 @@
                     <div class="card-body">
                         <h4 class="card-title">Engenharia Civil na Gestão da Construção Civíl, Tecnologia da Construção e Estruturas, e Ambiental em Recursos Hídricos</h4>
                         <h6 class="card-subtitle mb-2 text-muted"><span class="badge badge-primary">Engenharia Civil</span></h6>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+<!--                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                         <div class="bottom-links">
                             <a href="#" class="card-link">Card link</a>
                             <a href="#" class="card-link">Another link</a>
-                        </div>
+                        </div> -->
                     </div>
                 </div>
             </div>
@@ -65,11 +65,11 @@
                     <div class="card-body">
                         <h4 class="card-title">Gestão e Tecnologia da Informação e Conhecimento</h4>
                         <h6 class="card-subtitle mb-2 text-muted"><span class="badge badge-primary">Ciência da Computação</span></h6>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+<!--                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                         <div class="bottom-links">
                             <a href="#" class="card-link">Card link</a>
                             <a href="#" class="card-link">Another link</a>
-                        </div>
+                        </div> -->
                     </div>
                 </div>
             </div>
@@ -78,11 +78,11 @@
                     <div class="card-body">
                         <h4 class="card-title">Psicologia na Saúde e na Gestão Organizacional</h4>
                         <h6 class="card-subtitle mb-2 text-muted"><span class="badge badge-primary">Psicologia</span></h6>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+<!--                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                         <div class="bottom-links">
                             <a href="#" class="card-link">Card link</a>
                             <a href="#" class="card-link">Another link</a>
-                        </div>
+                        </div> -->
                     </div>
                 </div>
             </div>
